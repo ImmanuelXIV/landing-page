8ff0852314ae0ffe4137f14a3b1d08e996d611da
ImmanuelXIV <manuel.schwall@gmail.com>
2020-12-19 15:38:36 +0100

    feat: Add active-class to section when scrolling - first draft

--- js/app.js
+++ js/app.js
@@ -49,8 +49,44 @@ document.querySelector('#navbar__list').appendChild(fragment);
 
 
 // Add class 'active' to section when near top of viewport
+function distanceBottomToTop(elem) {
+    let distance = elem.getBoundingClientRect().bottom;
+    if (distance >= 0) {
+        return distance;
+    }
+    else {
+        // if distance is negative return very large positive number
+        return Infinity;
+    }
+}
 
+function toggleActiveSection() {
+    let distances = [];
+    for (let section of sections) {
+        distances.push(distanceBottomToTop(section));
+    }
 
+    //console.log(...distances);
+
+    // get index of smalles positive distance 
+    let idx = distances.indexOf(Math.min(...distances));
+    
+    for (let i = 0; i < sections.length; i++) {
+        if (i == idx) {
+            // add active class
+            if (!sections[idx].classList.contains('active-class')) {
+                sections[idx].classList.add('active-class');
+            }
+        }
+        else {
+            if (sections[i].classList.contains('classactive-')) {
+                sections[idx].classList.remove('active-class');
+            }
+        }
+    }    
+}
+
+window.onscroll = toggleActiveSection();
 // Scroll to anchor ID using scrollTO event
 
 
